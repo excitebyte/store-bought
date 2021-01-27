dbb5c888807adf95e8e92e7befaaa3ed7b456b63
excitebyte <46268226+excitebyte@users.noreply.github.com>
2021-01-27 01:32:13 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+	return (
+		<div className="App">
+			<h1>Store</h1>
+			<h1>Bought</h1>
+			<h4>make every purchase your best purchase</h4>
+		</div>
+	);
+}
